c6317f63f60ad9ef1fe1e39f880c1467797824cb
João Ferreira <jpfn2407@gmail.com>
2021-01-31 17:48:38 +0000

    FirstCommit

--- src/main/java/ual/views/GUI.java
+++ src/main/java/ual/views/GUI.java
@@ -1,11 +1,11 @@
 package ual.views;
+
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class GUI {
+public class GUI extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
