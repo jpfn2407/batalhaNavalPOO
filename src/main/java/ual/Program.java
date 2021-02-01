93690e7d02d54a265c8df79ef5350652554b2dad
João Ferreira <jpfn2407@gmail.com>
2021-02-01 00:55:49 +0000

    secondCommit

--- src/main/java/ual/Program.java
+++ src/main/java/ual/Program.java
@@ -3,6 +3,8 @@ import ual.views.*;
 
 public class Program {
     public static void main(String[] args) {
+        /*GUI gui = new GUI();
+        gui.main(args);*/
         new CLI();
     }
 }
--- src/main/java/ual/views/GUI.java
+++ src/main/java/ual/views/GUI.java
@@ -1,11 +1,43 @@
 package ual.views;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.TilePane;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-public class GUI extends Application {
-    @Override
-    public void start(Stage stage) throws Exception {
+import java.io.IOException;
 
+
+public class GUI extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/menu.fxml"));
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        stage.setTitle("Batalha Naval");
+        stage.setScene(new Scene(root,600, 400));
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
